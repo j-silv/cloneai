1ef05a1b6f1811cb052082150f2f62d95ef55315
Justin Silver <justinrsilver@gmail.com>
2025-02-01 22:59:32 -0800

    Add back in the silencedetect filter from ffmpeg
    Although not perfect, I think its the simplest thing to enable fast
    processing and I can always fix up things later. I have a min
    nonsilence length to remove any bits that I don't want.
    
    Although it didn't work that well for my simple test file. It might work better with
    the actual speaker. I think I can keep the minimum silence pretty low though and just
    make sure I filter out later with the nonsilence threshold

--- cloneai/split.py
+++ cloneai/split.py
@@ -6,6 +6,147 @@ import subprocess
 import matplotlib.pyplot as plt
 
 class FFmpeg:
+
+
+    def get_silences(infile, dB=-60, dur=2.0):
+        """Use ffmpeg to detect silence regions of audio
+        
+        This is based on an example using ffmpeg-python located here:
+        https://github.com/kkroening/ffmpeg-python/blob/master/examples/split_silence.py
+        
+        Some differences include avoiding ffmpeg-python library (raw subprocess calls)
+        and also just collecting the raw silence start/end timestamps instead of converting that 
+        to chunk_start and chunk_end times (this is done later)
+        """
+
+        cmd = ["ffmpeg", "-hide_banner",
+            "-i", infile,
+            "-filter_complex",
+            f"silencedetect=d={dur}:n={dB}dB",
+            "-f", "null", "-"
+        ]
+        # print(" ".join(cmd))
+
+        silence_start_re = re.compile(r' silence_start: (?P<start>[0-9]+(\.?[0-9]*))$')
+        silence_end_re = re.compile(r' silence_end: (?P<end>[0-9]+(\.?[0-9]*)) ')
+        total_duration_re = re.compile(r'size=[^ ]+ time=(?P<hours>[0-9]{2}):(?P<minutes>[0-9]{2}):(?P<seconds>[0-9\.]{5}) bitrate=')
+
+        result = subprocess.run(cmd, capture_output=True)
+        lines = result.stderr.decode("utf-8").splitlines()
+
+        starts = []
+        ends = []
+
+        for line in lines:
+
+            silence_start_match = silence_start_re.search(line)
+            silence_end_match = silence_end_re.search(line)
+            total_duration_match = total_duration_re.search(line)
+
+            if silence_start_match:
+                starts.append(float(silence_start_match.group('start')))
+            elif silence_end_match:
+                ends.append(float(silence_end_match.group('end')))
+            elif total_duration_match:
+                hours = int(total_duration_match.group('hours'))
+                minutes = int(total_duration_match.group('minutes'))
+                seconds = float(total_duration_match.group('seconds'))
+                end_time = hours * 3600 + minutes * 60 + seconds
+
+        assert len(starts) >= len(ends), "Less number of starts than ends should never occur"
+
+        # to assure that len(starts) == len(ends) for iteration later
+        # this handles ending with silence
+        if len(starts) > len(ends):
+            ends.append(end_time) 
+
+        return starts, ends, end_time
+    
+
+
+
+    def get_nonsilences(infile, dB=-60, min_silence=2.0, min_nonsilence=2):
+        """Convert all audio files to specific format and split for ML training
+        
+        Uses librosa and ffmpeg utility to load in audio files
+        and split on silence to detect sentence boundaries
+
+        ffmpeg usage is required because the raw audio files are in .aac format
+        and soundread (library that librosa uses for loading) does not support
+        MP3 and MP4 formats
+
+        Params:
+            config   : dict for controlling audio preprocessing
+                format       : convert to this audio format
+                sampling     : convert to this sampling rate
+                top_db       : threshold in decibels below reference to consider as silence
+                frame_length : the number of samples per analysis frame
+                hop_length   : the number of samples between anlysis frames
+
+        Also we could avoid using librosa (kind of unnecessary)
+        and just use ffmpeg and use the existing filter:
+        https://github.com/kkroening/ffmpeg-python/blob/master/examples/split_silence.py
+        https://ffmpeg.org/ffmpeg-filters.html
+        silencedetect 
+
+        that actually splits silence based on dB and a silence duration
+        then once we have that information we can output the right timestamps
+
+        there is also "silenceremove" filter but this doesn't split into
+        separate files which is what we want... we need the timestamps 
+        """
+
+        starts, ends, end_time = FFmpeg.get_silences(infile, dB, min_silence)
+
+        len_starts = len(starts)
+        len_ends = len(ends)
+
+        splits = []
+
+        assert len_starts == len_ends, "Silence detection did not work correctly because uneven lengths"
+
+        if len_starts == 0: # implicitly == len_ends
+            return splits
+        for i in range(len_starts):
+            if i == 0 and starts[0] != 0:
+                # handles non-silence at beginning of track
+                ss = 0.0
+                to = starts[0]
+            elif i+1 == len_starts:
+                # handles end of track (avoids out-of-range indexing)
+                if ends[i] > end_time:
+                    # because end_time is slightly different than the parsed time
+                    continue
+                ss = ends[i]
+                to = end_time
+            else:
+                ss = ends[i]
+                to = starts[i + 1]
+
+            if (to - ss) >= min_nonsilence:
+                # print(i, "|", starts[i], ends[i], "|", ss, to)
+                splits.append([ss, to])
+                
+        return splits
+
+    def get_duration(infile):
+        """Command to extract time duration of ffmpeg input file
+        
+        Interestingly this seemed to give a different value than the end_time 
+        obtained in the get_silences() function
+
+        This is because this just uses the metadata, but not the actual 
+        data which is processed when we calculate the silences
+        """
+        cmd = ["ffprobe",
+            "-i", infile,
+            "-show_entries", "format=duration",
+            "-v", "quiet", "-of", "csv=p=0",
+        ]
+        result = subprocess.run(cmd, capture_output=True)
+        duration = result.stdout.decode("utf-8").strip()
+        return float(duration)
+
     def get_sample_rate(filename):
         cmd = ["ffprobe",
             "-show_entries",
@@ -17,63 +158,27 @@ class FFmpeg:
         result = subprocess.run(cmd, capture_output=True, text=True, stdin=filename)
         sr = result.stdout.strip()
         return sr
-
-    def read_chunk(filename, chunk_size_s=5, hop_length_s=1.25, sr=16000, audio="s16le", verbose=False):
-        ss = 0
-
-        while(1):
-            cmd = ["ffmpeg",
-                "-v",
-                "verbose",
-                "-accurate_seek",
-                "-i",
-                filename,
-                "-f",
-                audio, # PCM signed 16-bit little-endian samples
-                "-ss",
-                str(ss),
-                "-t",
-                str(chunk_size_s),
-                "-ac",   # number of channels
-                "1",
-                "-ar",   # sampling rate (Hz)
-                str(sr), 
-                "pipe:"]
-            
-            result = subprocess.run(cmd, capture_output=True)
-            stdout = result.stderr.decode("utf-8")
-
-            if verbose:
-                print(stdout)
-
-            if re.search("Output file is empty", stdout):
-                break
-
-            yield result.stdout
-
-            ss += hop_length_s
     
-    def write(filename, out_buffer, sr=16000, audio="s16le", ss=None, to=None, verbose=False):
-        # TODO -> might need the -c copy option if out_format is same as in_format?
-        #         I'm not sure if ffmpeg is smart enough to not reencode 
+
+    def write(infile, outfile, ss=None, to=None, verbose=False):
         cmd = ["ffmpeg",
             "-v",
             "verbose",
             "-y", # overwrite files without asking
-            "-f",
-            audio,
-            "-ar",
-            str(sr),
             "-i",
-            "pipe:"]
+            infile]
+        
         if ss is not None and to is not None:
             cmd.extend(["-ss", str(ss), "-to", str(to)])
-        cmd.append(filename)
 
-        result = subprocess.run(cmd, capture_output=True, input=out_buffer)
+        cmd.append(outfile)
+
+        result = subprocess.run(cmd, capture_output=True)
 
         if verbose:
             print(result.stderr.decode("utf-8"))
+
+
 
 def matplotlib_plot(arr):
     plt.plot(arr)
@@ -89,32 +194,16 @@ def matplotlib_plot_with_intervals(arr, intervals):
     plt.show()
 
 if __name__ == "__main__":
-    chunk_size_s = 5
-    hop_length_s = 1.25
-    gen_chunks = FFmpeg.read_chunk("/home/justin/files/test_ffmpeg/in.aac",
-                                   chunk_size_s=chunk_size_s, hop_length_s=hop_length_s, verbose=False)
+    silences = (FFmpeg.get_silences("/home/justin/files/test_ffmpeg/in.wav", dB=-30, dur=1.5))
+    nonsilences = (FFmpeg.get_nonsilences("/home/justin/files/test_ffmpeg/in.wav", dB=-40, min_silence=1.0, min_nonsilence=1))
+
+    print(nonsilences)
+    for i, (start, end) in enumerate(nonsilences):
+        print(i, start, end)
+        outfile = f"/home/justin/files/test_ffmpeg/in_split_{i}.wav"
+        FFmpeg.write("/home/justin/files/test_ffmpeg/in.wav", outfile, ss=start, to=end, verbose=False)
 
 
-    concat_times = [] 
-    pos = 0
-    for i, chunk in enumerate(gen_chunks):
-        print(f"Processing chunk {i}")
-        arr = np.frombuffer(chunk, dtype="<i2")
-        splits_samples = librosa.effects.split(arr, top_db=60, frame_length=2048, hop_length=512)
-        splits_time = librosa.samples_to_time(splits_samples, sr=16000)
-
-        for (start, end) in splits_time:
-            print(start + pos, end + pos)
-        # print(splits_time)
-        
-        pos += hop_length_s
-
-        # min_length = 2
-        # mask = (splits_time[:,1]-splits_time[:,0]) > min_length
-        # splits_time_filtered = splits_time[mask]
-        # for i, (split_start, split_end) in enumerate(splits_time_filtered):
-        #     outfile = f"/home/justin/files/test_ffmpeg/in_split_{i}.aac"
-        #     FFmpeg.write(outfile, arr.tobytes(), ss=split_start, to=split_end)
 
 
 
