bbfbdc41845770b19ed10211cc0c88333e14cba5
Justin Silver <justinrsilver@gmail.com>
2025-02-02 10:50:12 -0800

    Scott test is working

--- cloneai/test.py
+++ cloneai/test.py
@@ -10,13 +10,26 @@ def test_split():
     print(f"{nonsilences=}")
 
     for i, (start, end) in enumerate(nonsilences):
-        outfile = f"./data/test/out_{i}.aac"
+        outfile = f"./data/test/in_{i}.aac"
         split.write(infile, outfile, ss=start, to=end, verbose=True, sample_rate_hz=44100)
 
     # ----------------------------------------------------------------------------------------
 
-    # infile = "./data/test/scott.aac"
-    # split.get_silences(infile, silence_db=-30, min_silence_s=1.0, verbose=True))
+    infile = "./data/test/scott.aac"
+    silences = split.get_silences(infile, silence_db=-30, min_silence_s=1.0, verbose=True)
+    # print(f"{silences=}")
+    nonsilences = split.convert_silences_to_nonsilences(*silences, min_nonsilence_s=3.0)
+    # print(f"{nonsilences=}")
+
+    for i, (start, end) in enumerate(nonsilences):
+        outfile = f"./data/test/scott_{i}.aac"
+        split.write(infile, outfile, ss=start, to=end, verbose=True, sample_rate_hz=44100)
+
+        if i == 10:
+            break
+
+    # ----------------------------------------------------------------------------------------
+
 
 
 
