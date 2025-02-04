836f39495cbcae5ce328fd3deea1d9d48bbe9927
Justin Silver <justinrsilver@gmail.com>
2025-02-03 22:34:56 -0800

    Change discord library to py-cord
    Better documentation, and slash commands are a lot simpler

--- cloneai/bot/voice.py
+++ cloneai/bot/voice.py
@@ -1,28 +1,37 @@
-# bot.py
 import os
-
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!', flush=True)
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'Logged on as {self.user}!', flush=True)
 
-    async def on_voice_state_update(self, member, before, after):
-        print(f"{member.name=} changed voice state", flush=True)
+#     async def on_voice_state_update(self, member, before, after):
+#         print(f"{member.name=} changed voice state", flush=True)
         
-        if (before.channel is None):
-            text_channel = member.guild.text_channels[-1]
-            names = ["**" + member.name + "**" for member in after.channel.members]
-            grammar = "are" if len(names) > 1 else "is"
-            await text_channel.send(f"@everyone, {', '.join(names)} {grammar} in the voice chat! Join voice channel: {after.channel.jump_url}")
+#         if (before.channel is None):
+#             text_channel = member.guild.text_channels[-1]
+#             names = ["**" + member.name + "**" for member in after.channel.members]
+#             grammar = "are" if len(names) > 1 else "is"
+#             await text_channel.send(f"@everyone, {', '.join(names)} {grammar} in the voice chat! Join voice channel: {after.channel.jump_url}")
 
-intents = discord.Intents.default()
-intents.voice_states = True
+# intents = discord.Intents.default()
+# intents.voice_states = True
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+# client = MyClient(intents=intents)
+# client.run(TOKEN)
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+bot.run(os.getenv('DISCORD_TOKEN'))
